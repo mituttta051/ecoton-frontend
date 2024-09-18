ad3b35977467c8079e4bcc73c86ec2faa5613712
mituttta051 <amityutneva@mail.ru>
2024-09-18 13:07:05 +0300

    feat: table component for events, participants and organizers

---
+++ src/utils/organizer-entity.ts
@@ -0,0 +1,10 @@
+// private Integer id;
+// private String name;
+// private String type;
+
+export interface OrganizerEntity {
+    id: number;
+    name: string;
+    type: string;
+}
+
---
+++ src/utils/participant-entity.ts
@@ -0,0 +1,13 @@
+// private Integer id;
+// private String name;
+// private String email;
+// private String gender;
+// private Integer age;
+
+export interface ParticipantEntity {
+    id: number;
+    name: string;
+    email: string;
+    gender: string;
+    age: number;
+}
